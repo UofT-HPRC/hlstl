24de42ca9549f1e1427629ac6db3e9d0bebe3e70
SeanNijjar <sean.nijjar@gmail.com>
2019-03-28 13:23:55 -0400

    Add debug helpers (in debug.hpp)
    Helpers are automatically enabled during CSIM and automatically disabled during CSYNTH. Alternatively,
    when compiling, a user can define DEBUG as a compiler option (-DDEBUG) to enable the checks.
    
    Implemented:
    - ASSERT(<condition>, <message>);
        Prints a message when the assertion triggers
    
    - ASSERT_IF (<when_to_check>,
                             <condition_to_check>,
                             <message>);
        An assertion that is only checked if <when_to_check>
        is true
    
    - UNIMPLEMENTED ()
        Automatically asserts when tripped
    
    - UNIMPLEMENTED_QUIET(<message>)
        When tripped, will print a message but won't assert
    
    - INFO (<message>)
        Prints a message
    
    - DO_PRAGMA (<pragma>)
        Places a pragma on the current line - use when you want
        macro expansion into a pragma

---
+++ common/debug.hpp
@@ -0,0 +1,77 @@
+#ifndef ASSERT_HPP
+#define ASSERT_HPP
+
+#include <cassert>
+
+#ifdef __RTL_SIMULATION__
+#define RTL_SIMULATION_DEFINED 1
+#else
+#define RTL_SIMULATION_DEFINED 0
+#endif
+#ifdef __SYNTHESIS__
+#define SYNTHESIS_DEFINED 1
+#else
+#define SYNTHESIS_DEFINED 0
+#endif
+#ifdef DEBUG
+#include <iostream>
+#define DEBUG_DEFINED 1
+#else
+#define DEBUG_DEFINED 0
+#endif
+
+//#if (defined __RTL_SIMULATION__ || defined DEBUG) && !defined __SYNTHESIS__
+#if (RTL_SIMULATION_DEFINED || DEBUG_DEFINED) && (SYNTHESIS_DEFINED == 0)
+  #define ASSERT(condition, message)\
+    if (!(condition)) {\
+        std::cout << "ASSERTION in " << __FILE__ << " at line " << __LINE__ << ": ";\
+        std::cout << message << std::endl;\
+        assert(false);\
+    }
+
+  #define ASSERT_IF(condition_when_to_check_assertion,assertion_condition,message)\
+    if ((condition_when_to_check_assertion)) {\
+      if (!(assertion_condition)) {\
+        std::cout << "ASSERTION in " << __FILE__ << " at line " << __LINE__ << ": ";\
+        std::cout << message << std::endl;\
+        assert(false);\
+      }\
+    }
+
+  #define UNIMPLEMENTED() \
+    std::cout << "UNIMPLEMENTED FUNCTION at line " << __LINE__ << " in file " << __FILE__ << std::endl;\
+    assert(false);
+
+  #define UNIMPLEMENTED_QUIET(message) \
+    std::cout << "UNIMPLEMENTED FUNCTION at line " << __LINE__ << " in file " << __FILE__ << std::endl;\
+    std::cout << "\t" << message << std::endl;
+
+  #define INFO(message) \
+    std::cout << message << std::endl;
+#else // DEBUG
+ #if defined(__clang__)
+ #define ASSERT(condition, message) \
+    DO_PRAGMA(clang diagnostic ignored "-Wunused-comparison")\
+    DO_PRAGMA(clang diagnostic ignored "-Wunused-value")\
+    condition;
+ #else 
+  #if defined(__GNUC__) || defined(__GNUG__)
+   #define ASSERT(condition, message) \
+    condition;
+  #endif
+ #endif
+  #define DO_PRAGMA_INNER(x) _Pragma (#x)
+  #define DO_PRAGMA(x) DO_PRAGMA_INNER(x)
+  #define ASSERT_IF(condition_when_to_check_assertion,assertion_condition,message) {}
+
+  #define INFO(message) {}
+  #define UNIMPLEMENTED() {}
+  #define UNIMPLEMENTED_QUIET(messsage) {}
+#endif // DEBUG
+
+const unsigned int ROUND_TO_NEXT_POWER_OF_2(const unsigned int value);
+    
+
+#define ADDRESS_ALIGN(address, byte_alignment) (address + ((byte_alignment - (address % byte_alignment) ) % byte_alignment))
+
+#endif
